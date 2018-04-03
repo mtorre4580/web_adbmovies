943d0d5c8a4ad77e27535b9f14a3d0f433658329
Matias <jflores90@gmail.com>
2018-04-03 12:14:55 -0300

    add library classname for component button.js
    Agrego la libreria classnames, para utilizarla en el button
    de acuerdo al type, agrega x clases ...

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import Proptypes from 'prop-types';
+import cx from 'classnames';
 
-const Button = ({icon,type,active,children,onClick}) => {
-    let styleButton = addStyleButton(type,active);
-    return(
-        <a onClick={onClick} className={styleButton} aria-label='Profile'>
-            { icon && <i className={icon} aria-hidden='true'></i>} {children}
-        </a>
-    );
-};
+const Button = ({icon,type,active,children,onClick}) => (
+    <a onClick={onClick} className={addClasses(type,active)} aria-label='Profile'>
+        { icon && <i className={icon} aria-hidden='true'></i>} {children}
+    </a>
+);
 
 export default Button;
 
-const addStyleButton = (type,active) => {
-    let styleButton = 'btn';
-    if(type === 'primary' || type === 'btn btn-primary'){
-        styleButton = `${styleButton} btn-primary`;
-    }
-    if(type === 'secondary'){
-        styleButton = `${styleButton} btn-secondary`;
-    }
-    if(type === 'light'){
-        styleButton = `${styleButton} btn-light`;
-    }
-    if(type === 'outline-dark'){
-        styleButton = `${styleButton} btn-outline-dark`;
-    }
-    if(active){
-        styleButton = `${styleButton} active`;
-    }
-    return styleButton;
-};
+const addClasses = (type,active) => cx({
+    'btn' : true,
+    'btn-primary' : type === 'primary',
+    'btn-secondary' : type === 'secondary',
+    'btn-light' : type === 'light',
+    'btn outline-dark' : type === 'outline-dark',
+    'active' : active
+});
 
 Button.propTypes = {
     children : Proptypes.any,
@@ -45,6 +32,3 @@ Button.defaultProps = {
     type : 'btn btn-primary',
     active : false
 };
-
-
-
